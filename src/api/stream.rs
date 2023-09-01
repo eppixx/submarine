7ead296efe875bb3a5f58227e4f52085119206df
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-01 18:14:14 +0200

    added route stream

--- src/api/stream.rs
+++ src/api/stream.rs
@@ -1,9 +1,37 @@
 use crate::Client;
 
 impl Client {
-    pub fn get_stream_url(&self, id: &str) -> String {
+    /// reference: http://www.subsonic.org/pages/api.jsp#stream
+    pub fn stream(
+        &self,
+        id: impl Into<String>,
+        max_bit_rate: Option<i32>,             // 0 for no limit
+        format: Option<impl Into<String>>,     // file ending, raw for disable
+        time_offset: Option<i64>,              // video only
+        size: Option<impl Into<String>>,       // video only in "widthxheight" format
+        estimate_content_length: Option<bool>, // restrict length
+        converted: Option<bool>,               // video only
+    ) -> String {
         let mut paras: std::collections::HashMap<&str, String> = self.auth.clone().into();
-        paras.insert("id", String::from(id));
+        paras.insert("id", id.into());
+        if let Some(bit_rate) = max_bit_rate {
+            paras.insert("maxBitRate", bit_rate.to_string());
+        }
+        if let Some(format) = format {
+            paras.insert("format", format.into());
+        }
+        if let Some(offset) = time_offset {
+            paras.insert("timeOffset", offset.to_string());
+        }
+        if let Some(size) = size {
+            paras.insert("size", size.into());
+        }
+        if let Some(content_length) = estimate_content_length {
+            paras.insert("estimateContentLength", content_length.to_string());
+        }
+        if let Some(converted) = converted {
+            paras.insert("converted", converted.to_string());
+        }
 
         let mut url: String = self.server_url.clone() + "/rest/stream?";
         for p in paras {
