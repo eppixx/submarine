6221704737d8758808cdda6f0a81ab86c68134a0
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-02 12:21:07 +0200

    changed routes star and unstar to be more closely to api

--- src/api/star.rs
+++ src/api/star.rs
@@ -1,32 +1,60 @@
-use std::fmt::Display;
+use crate::{
+    data::{Info, ResponseType},
+    Client, SubsonicError,
+};
 
-use crate::{Client, SubsonicError};
+impl Client {
+    /// reference: http://www.subsonic.org/pages/api.jsp#star
+    pub async fn star(
+        &self,
+        id: Vec<impl Into<String>>,
+        album_id: Vec<impl Into<String>>,
+        artist_id: Vec<impl Into<String>>,
+    ) -> Result<Info, SubsonicError> {
+        let mut paras: std::collections::HashMap<&str, String> = self.auth.clone().into();
+        for id in id {
+            paras.insert("id", id.into());
+        }
+        for id in album_id {
+            paras.insert("albumId", id.into());
+        }
+        for id in artist_id {
+            paras.insert("artistId", id.into());
+        }
 
-/// parameter for the star method
-pub enum StarTarget {
-    SongOrFolder,
-    Album,
-    Artist,
-}
-
-impl Display for StarTarget {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::SongOrFolder => write!(f, "id"),
-            Self::Album => write!(f, "albumId"),
-            Self::Artist => write!(f, "artistId"),
+        let body = self.request("star", Some(paras), None).await?;
+        if let ResponseType::Ping {} = body.data {
+            Ok(body.info)
+        } else {
+            Err(SubsonicError::Submarine(String::from(
+                "expected type Ping but found wrong type",
+            )))
         }
     }
 }
 
-impl Client {
-    /// Quelle: http://www.subsonic.org/pages/api.jsp#star
-    pub async fn star(&self, id: &str, target: StarTarget) -> Result<(), SubsonicError> {
-        let mut paras = std::collections::HashMap::new();
-        let key = target.to_string();
-        paras.insert(key.as_str(), String::from(id));
+#[cfg(test)]
+mod tests {
+    use crate::data::{OuterResponse, ResponseType};
 
-        let _ = self.request("star", Some(paras), None).await?;
-        Ok(())
+    #[test]
+    fn star_convert() {
+        let response_txt = r##"
+{
+    "subsonic-response": {
+        "status": "ok",
+        "version": "1.16.1",
+        "type": "navidrome",
+        "serverVersion": "0.49.3 (8b93962f)"
+    }
+}"##;
+        let response = serde_json::from_str::<OuterResponse>(response_txt)
+            .unwrap()
+            .inner;
+        if let ResponseType::Ping {} = response.data {
+            assert_eq!(&response.info.status, "ok");
+        } else {
+            panic!("wrong type");
+        }
     }
 }
--- src/api/unstar.rs
+++ src/api/unstar.rs
@@ -1,14 +1,60 @@
-use crate::api::star::StarTarget;
-use crate::{Client, SubsonicError};
+use crate::{
+    data::{Info, ResponseType},
+    Client, SubsonicError,
+};
 
 impl Client {
-    /// Quelle: http://www.subsonic.org/pages/api.jsp#unstar
-    pub async fn unstar(&self, id: &str, target: StarTarget) -> Result<(), SubsonicError> {
-        let mut paras = std::collections::HashMap::new();
-        let key = target.to_string();
-        paras.insert(key.as_str(), String::from(id));
+    /// reference: http://www.subsonic.org/pages/api.jsp#unstar
+    pub async fn unstar(
+        &self,
+        id: Vec<impl Into<String>>,
+        album_id: Vec<impl Into<String>>,
+        artist_id: Vec<impl Into<String>>,
+    ) -> Result<Info, SubsonicError> {
+        let mut paras: std::collections::HashMap<&str, String> = self.auth.clone().into();
+        for id in id {
+            paras.insert("id", id.into());
+        }
+        for id in album_id {
+            paras.insert("albumId", id.into());
+        }
+        for id in artist_id {
+            paras.insert("artistId", id.into());
+        }
 
-        let _ = self.request("unstar", Some(paras), None).await?;
-        Ok(())
+        let body = self.request("star", Some(paras), None).await?;
+        if let ResponseType::Ping {} = body.data {
+            Ok(body.info)
+        } else {
+            Err(SubsonicError::Submarine(String::from(
+                "expected type Ping but found wrong type",
+            )))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::data::{OuterResponse, ResponseType};
+
+    #[test]
+    fn unstar_convert() {
+        let response_txt = r##"
+{
+    "subsonic-response": {
+        "status": "ok",
+        "version": "1.16.1",
+        "type": "navidrome",
+        "serverVersion": "0.49.3 (8b93962f)"
+    }
+}"##;
+        let response = serde_json::from_str::<OuterResponse>(response_txt)
+            .unwrap()
+            .inner;
+        if let ResponseType::Ping {} = response.data {
+            assert_eq!(&response.info.status, "ok");
+        } else {
+            panic!("wrong type");
+        }
     }
 }
