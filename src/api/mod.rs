ac3fe4a6b477c6512d2cda6252900e6e8c7cee20
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-04 16:44:13 +0200

    added route scrobble

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -5,6 +5,7 @@ pub mod get_album;
 pub mod get_album_info;
 pub mod get_album_info2;
 pub mod get_album_list;
+pub mod scrobble;
 pub mod get_album_list2;
 pub mod get_artist;
 pub mod get_artist_info;
---
+++ src/api/scrobble.rs
@@ -0,0 +1,35 @@
+use crate::{
+    data::{Info, ResponseType},
+    Client, SubsonicError,
+};
+
+impl Client {
+    /// reference: http://www.subsonic.org/pages/api.jsp#scrobble
+    pub async fn scrobble(
+        &self,
+        id_at_time: Vec<(impl Into<String>, Option<usize>)>,
+        submission: Option<bool>, // defaults to true
+    ) -> Result<Info, SubsonicError> {
+        let mut paras: std::collections::HashMap<&str, String> = self.auth.clone().into();
+        for (id, time) in id_at_time {
+            paras.insert("id", id.into());
+            if let Some(time) = time {
+                paras.insert("time", time.to_string());
+            }
+        }
+        if let Some(submission) = submission {
+            paras.insert("submission", submission.to_string());
+        }
+
+        let body = self.request("scrobble", Some(paras), None).await?;
+        if let ResponseType::Ping {} = body.data {
+            Ok(body.info)
+        } else {
+            Err(SubsonicError::Submarine(String::from(
+                "expected type Ping but found wrong type",
+            )))
+        }
+    }
+}
+
+//TODO test
