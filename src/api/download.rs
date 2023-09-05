f42342afe97aea16db76da585a709ce3df1a998f
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-06 00:18:17 +0200

    added a function to download binary data

--- src/api/download.rs
+++ src/api/download.rs
@@ -1,8 +1,20 @@
-use crate::Client;
+use crate::{Client, SubsonicError};
 
 impl Client {
     /// reference: http://www.subsonic.org/pages/api.jsp#download
-    pub fn download(&self, id: impl Into<String>) -> String {
+    pub fn download_url(&self, id: impl Into<String>) -> String {
         format!("{}/rest/download?{}", self.server_url, id.into())
+    }
+
+    /// reference: http://www.subsonic.org/pages/api.jsp#download
+    pub async fn download(&self, id: impl Into<String>) -> Result<Vec<u8>, SubsonicError> {
+        let result = match reqwest::get(self.download_url(
+        id)).await {
+            Ok(result) => result,
+            Err(e) => return Err(SubsonicError::Connection(e)),
+        };
+
+        let bytes = result.bytes().await?.to_vec();
+        Ok(bytes)
     }
 }
