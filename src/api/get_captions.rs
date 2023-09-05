0b323affd0afb66389ea11a3f293771a7e8c59f1
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-06 00:52:50 +0200

    changed function name; added funciton for getting binary

--- src/api/get_captions.rs
+++ src/api/get_captions.rs
@@ -1,8 +1,12 @@
-use crate::Client;
+use crate::{Client, SubsonicError};
 
 impl Client {
     /// reference: http://www.subsonic.org/pages/api.jsp#getCatptions
-    pub fn get_captions(&self, id: impl Into<String>, format: Option<impl Into<String>>) -> String {
+    pub fn get_captions_url(
+        &self,
+        id: impl Into<String>,
+        format: Option<impl Into<String>>,
+    ) -> String {
         let mut paras = std::collections::HashMap::new();
         paras.insert("id", id.into());
         if let Some(format) = format {
@@ -15,5 +19,20 @@ impl Client {
         }
 
         url
+    }
+
+    /// reference: http://www.subsonic.org/pages/api.jsp#getCatptions
+    pub async fn get_captions(
+        &self,
+        id: impl Into<String>,
+        format: Option<impl Into<String>>,
+    ) -> Result<Vec<u8>, SubsonicError> {
+        let result = match reqwest::get(self.get_captions_url(id, format)).await {
+            Ok(result) => result,
+            Err(e) => return Err(SubsonicError::Connection(e)),
+        };
+
+        let bytes = result.bytes().await?.to_vec();
+        Ok(bytes)
     }
 }
