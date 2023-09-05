404df8c9fe1aa376a9c8dcc3c026903e9e7f2444
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-06 00:42:09 +0200

    changed function name; fixed parameter missing auth; added fn for downloading binary

--- src/api/stream.rs
+++ src/api/stream.rs
@@ -1,8 +1,8 @@
-use crate::Client;
+use crate::{Client, SubsonicError};
 
 impl Client {
     /// reference: http://www.subsonic.org/pages/api.jsp#stream
-    pub fn stream(
+    pub fn stream_url(
         &self,
         id: impl Into<String>,
         max_bit_rate: Option<i32>,             // 0 for no limit
@@ -13,6 +13,7 @@ impl Client {
         converted: Option<bool>,               // video only
     ) -> String {
         let mut paras = std::collections::HashMap::new();
+        self.auth.add_parameter(&mut paras);
         paras.insert("id", id.into());
         if let Some(bit_rate) = max_bit_rate {
             paras.insert("maxBitRate", bit_rate.to_string());
@@ -39,5 +40,35 @@ impl Client {
         }
 
         url
+    }
+
+    /// reference: http://www.subsonic.org/pages/api.jsp#stream
+    pub async fn stream(
+        &self,
+        id: impl Into<String>,
+        max_bit_rate: Option<i32>,             // 0 for no limit
+        format: Option<impl Into<String>>,     // file ending, raw for disable
+        time_offset: Option<i64>,              // video only
+        size: Option<impl Into<String>>,       // video only in "widthxheight" format
+        estimate_content_length: Option<bool>, // restrict length
+        converted: Option<bool>,               // video only
+    ) -> Result<Vec<u8>, SubsonicError> {
+        let result = match reqwest::get(self.stream_url(
+            id,
+            max_bit_rate,
+            format,
+            time_offset,
+            size,
+            estimate_content_length,
+            converted,
+        ))
+        .await
+        {
+            Ok(result) => result,
+            Err(e) => return Err(SubsonicError::Connection(e)),
+        };
+
+        let bytes = result.bytes().await?.to_vec();
+        Ok(bytes)
     }
 }
