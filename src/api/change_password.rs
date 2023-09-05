3611f6e80c4e7286d85bfd62f200f80d93098717
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-05 17:51:31 +0200

    added route change_password

---
+++ src/api/change_password.rs
@@ -0,0 +1,28 @@
+use crate::{
+    data::{Info, ResponseType},
+    Client, SubsonicError,
+};
+
+impl Client {
+    /// reference: http://www.subsonic.org/pages/api.jsp#changePassword
+    pub async fn change_password(
+        &self,
+        username: impl Into<String>,
+        password: impl Into<String>,
+    ) -> Result<Info, SubsonicError> {
+        let mut paras: std::collections::HashMap<&str, String> = self.auth.clone().into();
+        paras.insert("username", username.into());
+        paras.insert("password", password.into());
+
+        let body = self.request("changePassword", Some(paras), None).await?;
+        if let ResponseType::Ping {} = body.data {
+            Ok(body.info)
+        } else {
+            Err(SubsonicError::Submarine(String::from(
+                "expected type Ping but found wrong type",
+            )))
+        }
+    }
+}
+
+//TODO add test
