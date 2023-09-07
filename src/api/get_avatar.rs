2e6b2923057aa4c979e18b6116e4725f71474e0e
Thomas Eppers <epperst@hochschule-trier.de>
2023-09-07 08:24:35 +0200

    fixed missing parameter in get_avatar; changed return type to Url; added test

--- src/api/get_avatar.rs
+++ src/api/get_avatar.rs
@@ -2,21 +2,17 @@ use crate::{Client, Parameter, SubsonicError};
 
 impl Client {
     /// reference: http://www.subsonic.org/pages/api.jsp#getAvatar
-    pub fn get_avatar_url(&self, username: impl Into<String>) -> String {
+    pub fn get_avatar_url(&self, username: impl Into<String>) -> Result<url::Url, url::ParseError> {
         let mut paras = Parameter::new();
+        self.auth.add_parameter(&mut paras);
         paras.push("username", username);
 
-        let mut url: String = self.server_url.clone() + "/rest/getAvatar?";
-        for p in paras.0 {
-            url += &("&".to_owned() + &p.0 + "=" + &p.1);
-        }
-
-        url
+        url::Url::parse_with_params(&format!("{}/rest/getAvatar", self.server_url), paras.0)
     }
 
     /// reference: http://www.subsonic.org/pages/api.jsp#getAvatar
     pub async fn get_avatar(&self, username: impl Into<String>) -> Result<Vec<u8>, SubsonicError> {
-        let result = match reqwest::get(self.get_avatar_url(username)).await {
+        let result = match reqwest::get(self.get_avatar_url(username)?).await {
             Ok(result) => result,
             Err(e) => return Err(SubsonicError::Connection(e)),
         };
@@ -26,4 +22,18 @@ impl Client {
     }
 }
 
-//TODO add test
+#[cfg(test)]
+mod tests {
+    use crate::{auth::AuthBuilder, Client};
+
+    #[tokio::test]
+    async fn create_get_avatar_url() {
+        let auth = AuthBuilder::new("peter", "v0.16.1")
+            ._salt("")
+            .hashed("change_me_password");
+        let client = Client::new("https://target.com", auth);
+        let url = client.get_avatar_url("name").unwrap();
+
+        assert_eq!("https://target.com/rest/getAvatar?u=peter&v=v0.16.1&c=submarine-lib&t=d4a5b2db9781fba37ec95f0312ade67a&s=&f=json&username=name", &url.to_string());
+    }
+}
